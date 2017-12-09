d5096d191fce7ddedcdfc9f22fae438c0664c88b
ShadyHany <33374818+ShadyHany@users.noreply.github.com>
2017-12-09 16:44:14 +0200

    Update shutdown.py

--- shutdown.py
+++ shutdown.py
@@ -1,22 +1,35 @@
 from tkinter import *
 import subprocess
 
+time=60
+
 def shutdown(event):
+    getTime()
     global time
     subprocess.call(["shutdown", "-s","-f" ,"-t", "{}".format(time)])
     
 def restart(event):
-    subprocess.call(["shutdown", "-r", "-t", "600"])
+    getTime()
+    global time 
+    subprocess.call(["shutdown", "-r", "-t", "{}".format(time)])
     
 def abort(event):
     subprocess.call(["shutdown", "-a"])
 
+def getTime():
+    global time
+    ttext=text.get()
+    time=int(ttext)
+    return
+
 def Exit(event):
-    root.destroy()
+    exit()
     
     
 root = Tk()
 root.title('Shutdown')
+
+text=StringVar()
 
 f1 =Frame(root,width=200,height=200,bg='lightblue')
 f2 =Frame(root,width=200,height=130,bg='lightblue')
@@ -24,12 +37,11 @@ b1 =Button(f1,text='shutdown after:',fg='red', bg ='white')
 b2 =Button(f1,text='restart after:',fg='blue',bg ='white')
 b3 =Button(f1,text='abort:',fg='green',bg ='white')
 b5 =Button(f1,text='Exit',fg='purple',bg ='white')
-entry=Entry(f2,width=10)
+entry=Entry(f2,width=10,textvariable=text)
 l1= Label(f2,text='Enter The Time In Minute:',bg='lightblue')
 
 l1.grid(row = 0, column=0,pady=18)
 entry.grid(padx=50,pady=28)
-
 f2.pack(side=RIGHT)
 f1.pack(fill=BOTH)
 b1.pack(fill=BOTH)
@@ -37,11 +49,10 @@ b2.pack(fill=BOTH)
 b3.pack(fill=BOTH)
 b5.pack(fill=BOTH)
 
-time = entry.get()*60
-
 b1.bind("<Button-1>",shutdown)
 b2.bind("<Button-1>",restart)
 b3.bind("<Button-1>",abort)
 b5.bind("<Button-1>",Exit)
 
 root.mainloop()
+
