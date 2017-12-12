aa8ec8100d879827cf4ce7f902428cc22c43925d
ShadyHany <ShadyHany@users.github.com>
2017-12-12 21:09:32 +0200

    handling type error

--- shutdown.py
+++ shutdown.py
@@ -1,29 +1,54 @@
 from tkinter import *
+from tkinter import messagebox
 import subprocess
 
 time=60
 
 def shutdown(event):
-    getTime()
     global time
-    subprocess.call(["shutdown", "-s","-f" ,"-t", "{}".format(time)])
+    if getTime() == 1:
+        root.mainloop()
+        return
+    subprocess.call(["shutdown", "-s","-f" ,"-t", "{}".format(time*60)])
+    Time_Of_Shutdown('shutdown')
     
 def restart(event):
-    getTime()
-    global time 
-    subprocess.call(["shutdown", "-r", "-t", "{}".format(time)])
+    global time
+    if getTime() == 1:
+        root.mainloop()
+        return
+    subprocess.call(["shutdown", "-r", "-t", "{}".format(time*60)])
+    Time_Of_Shutdown('restart')
     
 def abort(event):
     subprocess.call(["shutdown", "-a"])
-
+    l2.config(text='User aborted the operation')
+    
 def getTime():
     global time
     ttext=text.get()
-    time=int(ttext)
-    return
+    try:
+        time=int(ttext)
+    except:
+        messagebox.showerror('Error','Please, enter an integer!')
+        entry.delete(0,END)
+        return 1
+
+def Time_Of_Shutdown(operation):
+    if entry.get() == '':
+        return
+    else:
+        l2.config(text='Your Computer Will {} after: {} MIN'.format(operation,time))
+        entry.delete(0,END)
+        return
 
 def Exit(event):
-    exit()
+    msg=messagebox.askyesno('Exit','Are you sure you want exit?')
+    if msg is True:
+        root.destroy()
+    else:
+        root.mainloop()
+
     
     
 root = Tk()
@@ -32,17 +57,21 @@ root.title('Shutdown')
 text=StringVar()
 
 f1 =Frame(root,width=200,height=200,bg='lightblue')
-f2 =Frame(root,width=200,height=130,bg='lightblue')
 b1 =Button(f1,text='shutdown after:',fg='red', bg ='white')
 b2 =Button(f1,text='restart after:',fg='blue',bg ='white')
 b3 =Button(f1,text='abort:',fg='green',bg ='white')
 b5 =Button(f1,text='Exit',fg='purple',bg ='white')
-entry=Entry(f2,width=10,textvariable=text)
-l1= Label(f2,text='Enter The Time In Minute:',bg='lightblue')
+entry=Entry(root,width=10,textvariable=text)
+l1= Label(root,text='Enter The Time In Minute:')
 
-l1.grid(row = 0, column=0,pady=18)
-entry.grid(padx=50,pady=28)
-f2.pack(side=RIGHT)
+f2=Frame(root)
+l2=Label(f2,text='Enter Time!',relief=GROOVE)
+
+f2.pack(side=BOTTOM)
+entry.pack(side=RIGHT)
+l1.pack(side=RIGHT)
+l2.pack(side=BOTTOM)
+
 f1.pack(fill=BOTH)
 b1.pack(fill=BOTH)
 b2.pack(fill=BOTH)
